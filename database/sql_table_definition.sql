e30f5a661697087334c68b80c2538a1292d2f5c6
Todd de Quincey <todddequincey@Todds-MacBook-Air.local>
2019-06-18 08:30:49 +0100

    sql definition + data cleaning

--- database/sql_table_definition.sql
+++ database/sql_table_definition.sql
@@ -1,53 +1,54 @@
+-- Create database
+CREATE DATABASE `globalwarming`;
+USE `globalwarming`;
+
 -- Create table with list of countries and their IDs
 CREATE TABLE `Countries` (
   `CountryId` CHAR(2) NOT NULL,
   `CountryName` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`CountryId`),
   UNIQUE KEY `CountryId` (`CountryId`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Create table with weather stations
 CREATE TABLE `StationDetails` (
-  `StationId` VARCHAR(6) NOT NULL,
-  `WBAN` INT(6) NOT NULL,
+  `StationId` VARCHAR(12) NOT NULL,
   `StationName` VARCHAR(100) DEFAULT NULL,
-  `CountryId` CHAR(2) DEFAULT NULL,
-  `ICAO` VARCHAR(8) DEFAULT NULL,
+  `Elevation` DECIMAL(6,1) DEFAULT NULL,
+  `StartDate` DATE DEFAULT NULL,
+  `EndDate` DATE DEFAULT NULL,
+  `City` VARCHAR(100) NOT NULL,
+  `State` VARCHAR(100) DEFAULT NULL,
+  `Country` VARCHAR(100) NOT NULL,
   `Lat` DECIMAL(10,8) DEFAULT NULL, 
   `Lon` DECIMAL(11,8) DEFAULT NULL, 
-  `Elevation` DECIMAL(5,1) DEFAULT NULL,
-  `StartDate` DATE DEFAULT NULL,
-  `EndDate` DATE DEFAULT NULL
   PRIMARY KEY (`StationId`),
   UNIQUE KEY `StationId` (`StationId`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Create table with daily weather station records
+-- Create table with monthly weather station records
 CREATE TABLE `StationRecords` (
-  `StationId` varchar(6) NOT NULL,
-  `WBAN` int(6) NOT NULL,
-  `Date` date NOT NULL,
-  `Temp` decimal(4,1) DEFAULT NULL,
-  `TempPoints` int(11) DEFAULT NULL,
-  `Dew` decimal(5,1) DEFAULT NULL,
-  `DewPoints` int(11) DEFAULT NULL,
-  `SLP` decimal(5,1) DEFAULT NULL,
-  `SLPPoints` int(11) DEFAULT NULL,
-  `StationPressure` decimal(5,1) DEFAULT NULL,
-  `StationPressurePoints` int(11) DEFAULT NULL,
-  `Visib` decimal(5,1) DEFAULT NULL,
-  `VisibPoints` int(11) DEFAULT NULL,
-  `WindSpeed` decimal(5,1) DEFAULT NULL,
-  `WindSpeedPoints` int(11) DEFAULT NULL,
-  `MaxWindSpeed` decimal(5,1) DEFAULT NULL,
-  `Gust` decimal(5,1) DEFAULT NULL,
-  `MaxTemp` decimal(5,1) DEFAULT NULL,
-  `MinTemp` decimal(5,1) DEFAULT NULL,
-  `Precip` decimal(5,2) DEFAULT NULL,
-  `SnowDepth` decimal(5,1) DEFAULT NULL,
-  `Conditions` decimal(7,1) DEFAULT NULL,
-  PRIMARY KEY (`StationId`),
-  UNIQUE KEY `StationId` (`StationId`) USING BTREE
+  `StationId` varchar(12) NOT NULL,
+  `Year` int(4) NOT NULL,
+  `Month` int(2) NOT NULL,
+  `Temp` decimal(6,3) DEFAULT NULL,
+  `Dew` decimal(7,3) DEFAULT NULL,
+  `SLP` decimal(7,3) DEFAULT NULL,
+  `StationPressure` decimal(7,3) DEFAULT NULL,
+  `Visib` decimal(7,3) DEFAULT NULL,
+  `WindSpeed` decimal(7,3) DEFAULT NULL,
+  `MaxWindSpeed` decimal(7,3) DEFAULT NULL,
+  `Gust` decimal(7,3) DEFAULT NULL,
+  `AvgMaxTemp` decimal(7,3) DEFAULT NULL,
+  `AvgMinTemp` decimal(7,3) DEFAULT NULL,
+  `Precip` decimal(7,3) DEFAULT NULL,
+  `SnowDepth` decimal(7,3) DEFAULT NULL,
+  `MaxTemp` decimal(7,3) DEFAULT NULL,
+  `MaxMinTemp` decimal(7,3) DEFAULT NULL,
+  `MinMaxTemp` decimal(7,3) DEFAULT NULL,
+  `MinTemp` decimal(7,3) DEFAULT NULL,
+  `NumberDailyRecords` int(2) DEFAULT NULL,
+  PRIMARY KEY (`StationId`,`Year`,`Month`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Create table with CO2 emission data
